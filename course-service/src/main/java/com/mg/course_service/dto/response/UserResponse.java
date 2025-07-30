8a02125e934fa7fdc2f57a2ceafd87e28fd8db55
Mahabbat Gozalov <mgzlovcode@gmail.com>
2025-07-30 19:43:40 +0400

    UserInfoResponse deleted using only UserResponse

--- course-service/src/main/java/com/mg/course_service/dto/response/UserResponse.java
+++ course-service/src/main/java/com/mg/course_service/dto/response/UserResponse.java
@@ -3,7 +3,6 @@ package com.mg.course_service.dto.response;
 import java.time.LocalDate;
 import java.util.UUID;
 
-//TODO: GENDER AND ROLE THERE IS NOT HERE. BE SURE ABOUT THAT, THIS IS WORKING.
 public record UserResponse(
         UUID id,
         String username,
